e65595f13859e5783c88f72277b97ae5431c09f5
Terence Ho <terencehwy@gmail.com>
2021-05-09 16:45:34 +0800

    Add component Hello.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.css'; // needs to specify filepath
+// import App from './App'; // if no extension specified, assume is .js file
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons'; // installed via npm. Useful classes for HTML tags
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// 'greeting' is a property that we can add for class tags
+// this will be available in Hello.js for us to use
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Hello greeting={'React Ninja'}/>,
   document.getElementById('root')
 );
 
