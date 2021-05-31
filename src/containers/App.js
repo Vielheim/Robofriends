3e5e3d366675f9eb117769facdcfd6db58572817
Terence Ho <terencehwy@gmail.com>
2021-05-31 11:24:09 +0800

    Convert page to use react hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import CardList from '../components/CardList';
 // import {robots} from './robots'; // need to destructure because robots.js can contain multiple objects
 import SearchBox from '../components/SearchBox'; 
@@ -6,63 +6,77 @@ import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css'
 
-class App extends Component {
+function App() {
 
+    // ------------------------- Not applicable for hooks ----------------------
     // order of execution: 
     // constructor
     // componentMount
     // render
     // componentDidMount
 
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchField: ''
-        }
-    }
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         robots: [],
+    //         searchField: ''
+    //     }
+    // }
 
-    componentDidMount() {
-        // update the state of robots here
+    // componentDidMount() {
+    //     // update the state of robots here
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //         .then(response => response.json())
+    //         .then(users => this.setState({robots: users}));
+
+    //     // fetch does http fetch
+    //     // when there is a change to state/virtual DOM, render() will be executed again
+    // }
+    // ---------------------------------------------------------------------------
+
+    // robots is the state, setRobots updates the state
+    const [robots, setRobots] = useState([]); // initial state of the object
+    const [searchField, setSearchField] = useState("");
+
+    // side effects: everytime react wants to render, it will also run the side effects
+    // it receives a second argument: the empty list
+    // without it, useEffect will keep running because render keeps getting called
+    // then when render gets called, it will fetch again via useEffect. --> infinite loop
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({robots: users}));
-
-        // fetch does http fetch
-        // when there is a change to state/virtual DOM, render() will be executed again
-    }
+            .then(users => setRobots(users));
+    }, []) // <- only run useEffect when the stuff inside this list changes
 
     // how to have dynamic updates to the robot states:
     // pass this listener into the searchbox and update from there
     // note that we have to use the anonymous function way to allow the reference of App to be saved in the function
     // if the fn is passed to SearchBox, the 'this' in the function may not refer to App anymore
-    onSearchChange = (event) => {
+    const onSearchChange = (event) => {
         // update searchField
-        this.setState({searchField: event.target.value});
+        setSearchField(event.target.value);
     }
 
-    render() {
-        // filter robots
-        const filteredRobots = this.state.robots.filter(
-            robot => robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    // filter robots
+    const filteredRobots = robots.filter(
+        robot => robot.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+    if (robots.length === 0) {
+        return <h1 className='f1 tc'>Loading...</h1>
+    } else {
+        return (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends!</h1>
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll> 
+                    <ErrorBoundary>
+                        <CardList robots={filteredRobots}/>
+                    </ErrorBoundary>
+                </Scroll>
+            </div>
         )
-        if (this.state.robots.length === 0) {
-            return <h1 className='f1 tc'>Loading...</h1>
-        } else {
-            return (
-                <div className='tc'>
-                    <h1 className='f1'>RoboFriends!</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll> 
-                        <ErrorBoundary>
-                            <CardList robots={filteredRobots}/>
-                        </ErrorBoundary>
-                    </Scroll>
-                </div>
-            )
-        }
-        // Scroll: scroll makes the cardlist of filtered robots scrollable
     }
+    // Scroll: scroll makes the cardlist of filtered robots scrollable
 }
 
 export default App;
